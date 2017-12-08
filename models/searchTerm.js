e529061e581180cdc3634c8f341f10cce1df36e1
Mack Miller <mack.matt.miller@gmail.com>
2017-12-08 13:07:03 -0600

    first draft

--- models/searchTerm.js
+++ models/searchTerm.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose'),
 Schema         = mongoose.Schema;
 
-const search
+const searchTermSchema = new Schema(
+		{
+			searchVal: String,
+			searchDate: Date
+		},
+		{timestamp: true}
+	);
+
+const ModelClass = mongoose.model('searchTerm', searchTermSchema);
+
+module.exports = ModelClass;
--- server.js
+++ server.js
@@ -6,18 +6,57 @@ app           = express(),
 bodyParser    = require('body-parser'),
 cors          = require('cors'),
 mongoose      = require('mongoose'),
-Bing          = require('node-bing-api')({accKey: ''});
+//util          = require('util'),
+Bing          = require('node-bing-api')(
+		{
+			accKey: '4b56815dc5be40eda4d1621864ce9223',
+		}
+	),
+searchTerm    = require('./models/searchTerm.js');
 
 app.use(bodyParser.json());
 app.use(cors());
+mongoose.connect('mongodb://admin:admin@ds133876.mlab.com:33876/img-search', {
+	useMongoClient: true
+});
 
+app.get('/api/latestsearches', (req, res, next) => {
+	searchTerm.find({}, (err, data) => {
+		res.json(data);
+	});
+});
+              
 app.get('/api/imagesearch/:searchVal*', (req, res, next)=>{
 	var { searchVal } = req.params;
 	var {offset} = req.query;
 
-	return res.json({
+	var data = new searchTerm({
 		searchVal,
-		offset
+		searchDate: new Date()
+	});
+
+	// Save to latest searches
+	data.save(err => {
+		if (err) {
+			res.send('Error');
+		}
+
+	});
+
+	Bing.images(searchVal, {
+		count: 10
+	}, function(error, response, body) {
+		var bingData=[];
+		
+		for(let i=0; i<10; i++) {
+			bingData.push({
+				url: body.value[i].webSearchUrl,
+				snippet: body.value[i].name,
+				thumbnail: body.value[i].thumbnailUrl,
+				context: body.value[i].hostPageDisplayUrl
+			});
+		}
+		res.json(bingData);
 	});
 });
 
